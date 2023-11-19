9f60f3514ab89a0e91d789e815d67fe6682515b9
pan yongcan <panyongcan@incostar.com>
2023-11-19 21:44:23 +0800

    add and update

---
+++ web/run_zhixing.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+PYTHON=""
+# python程序位置，可搭配一键包或是省去每次切换环境
+
+#while true
+#do
+#    if [ -z "$PYTHON" ]; then
+#        CUDA_VISIBLE_DEVICES=1 python wenda.py -t llama
+#    else
+#        CUDA_VISIBLE_DEVICES=1 $PYTHON wenda.py -t llama
+#    fi
+#sleep 1
+#done
+CUDA_VISIBLE_DEVICES=1 python wenda.py -t zhixing
+#python wenda.py -t llama
+#python wenda.py -t llama
